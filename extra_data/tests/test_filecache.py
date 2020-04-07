b26d6f286163b8b177c27a4cb52388fcf962e9dc
Egor Sobolev <egor.sobolev@xfel.eu>
2020-04-07 14:59:19 +0200

    Added estimation of the number of files in filecache test

--- extra_data/tests/test_filecache.py
+++ extra_data/tests/test_filecache.py
@@ -5,25 +5,19 @@ from extra_data.reader import DataCollection
 
 @pytest.fixture
 def filecache_512():
-    from extra_data.filecache import extra_data_filecache
-    orig_cache = extra_data_filecache._cache
-    orig_maxfiles = extra_data_filecache.maxfiles
-    extra_data_filecache._cache = OrderedDict()
-    extra_data_filecache._maxfiles = 512
-    yield extra_data_filecache
-    extra_data_filecache._cache = orig_cache
-    extra_data_filecache._maxfiles = orig_maxfiles
+    from extra_data.filecache import FileCache, set_global_filecache, get_global_filecache
+    orig_cache = get_global_filecache()
+    set_global_filecache(FileCache(512))
+    yield get_global_filecache()
+    set_global_filecache(orig_cache)
 
 @pytest.fixture
 def filecache_3():
-    from extra_data.filecache import extra_data_filecache
-    orig_cache = extra_data_filecache._cache
-    orig_maxfiles = extra_data_filecache.maxfiles
-    extra_data_filecache._cache = OrderedDict()
-    extra_data_filecache._maxfiles = 3
-    yield extra_data_filecache
-    extra_data_filecache._cache = orig_cache
-    extra_data_filecache._maxfiles = orig_maxfiles
+    from extra_data.filecache import FileCache, set_global_filecache, get_global_filecache
+    orig_cache = get_global_filecache()
+    set_global_filecache(FileCache(3))
+    yield get_global_filecache()
+    set_global_filecache(orig_cache)
 
 
 def test_filecache_large(mock_spb_raw_run, filecache_512):
@@ -32,6 +26,18 @@ def test_filecache_large(mock_spb_raw_run, filecache_512):
     files = [os.path.join(mock_spb_raw_run, f) \
              for f in os.listdir(mock_spb_raw_run) if f.endswith('.h5')]
     run = DataCollection.from_paths(files)
+    
+    # estimate the number of files which should be open to read the first train
+    cnt, fix = {}, {}
+    for f in run.files:
+        a, b = os.path.basename(f.filename).rsplit('-',1)
+        try:
+            cnt[a] += 1
+        except KeyError:
+            cnt[a] = 1
+        if b == 'S00000.h5':
+            fix[a] = cnt[a] 
+    nfiles = sum(a for a in fix.values())
 
     trains_iter = run.trains()
     tid, data = next(trains_iter)
@@ -39,10 +45,10 @@ def test_filecache_large(mock_spb_raw_run, filecache_512):
     device = 'SPB_IRU_CAM/CAM/SIDEMIC:daqOutput'
     assert device in data
     assert data[device]['data.image.pixels'].shape == (1024, 768)
-    assert len(fc._cache) == len(run.files)
+    assert len(fc._cache) == nfiles
 
     del run, trains_iter
-    assert len(fc._cache) == 0    
+    assert len(fc._cache) == 0
 
 def test_filecache_small(mock_spb_raw_run, filecache_3):
     fc = filecache_3
