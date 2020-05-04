1f40e7405be5fa28d12d41b8e3ce5eac3d8de220
Thomas Michelat <thomas.michelat@xfel.eu>
2020-05-04 18:43:31 +0200

    use ServerInThread from karabo_bridge package

--- extra_data/export.py
+++ extra_data/export.py
@@ -10,16 +10,11 @@ You should have received a copy of the 3-Clause BSD License along with this
 program. If not, see <https://opensource.org/licenses/BSD-3-Clause>
 """
 
-from functools import partial
 from argparse import ArgumentParser
 import os.path as osp
-from queue import Empty, Queue
-from socket import gethostname
-from threading import Thread
 from warnings import warn
 
-from karabo_bridge import serialize
-import zmq
+from karabo_bridge import ServerInThread
 
 from .reader import RunDirectory, H5File
 from .stacking import stack_detector_data
@@ -28,140 +23,15 @@ from .utils import find_infiniband_ip
 __all__ = ['ZMQStreamer', 'serve_files']
 
 
-class ZMQStreamer(Thread):
+class ZMQStreamer(ServerInThread):
     def __init__(self, port, sock='REP', maxlen=10, protocol_version='2.2',
-                 dummy_timestamps=False, use_infiniband=False):
-        """ZeroMQ interface sending data over a TCP socket.
+                 dummy_timestamps=False):
+        warn("Please use :ref:karabo_bridge.ServerInThread instead",
+             DeprecationWarning, stacklevel=2)
 
-        example::
-
-            # Server:
-            serve = ZMQStreamer(1234)
-            serve.start()
-
-            for tid, data in run.trains():
-                result = important_processing(data)
-                serve.feed(result)
-
-            # Client:
-            from karabo_bridge import Client
-            client = Client('tcp://server.hostname:1234')
-            data = client.next()
-
-        Parameters
-        ----------
-        port: int
-            Local TCP port to bind socket to
-        maxlen: int, optional
-            How many trains to cache before sending (default: 10)
-        protocol_version: ('1.0' | '2.1')
-            Which version of the bridge protocol to use. Defaults to the latest
-            version implemented.
-        dummy_timestamps: bool
-            Some tools (such as OnDA) expect the timestamp information to be in
-            the messages. We can't give accurate timestamps where these are not
-            in the file, so this option generates fake timestamps from the time
-            the data is fed in.
-        use_infiniband: bool
-            Use infiniband interface if the host has one (default False)
-        """
-        super().__init__()
-        self.serialize = partial(serialize, protocol_version=protocol_version,
-                                 dummy_timestamps=dummy_timestamps)
-
-        self.zmq_context = zmq.Context()
-        if sock == 'REP':
-            self.server_socket = self.zmq_context.socket(zmq.REP)
-        elif sock == 'PUB':
-            self.server_socket = self.zmq_context.socket(zmq.PUB)
-        elif sock == 'PUSH':
-            self.server_socket = self.zmq_context.socket(zmq.PUSH)
-        else:
-            raise ValueError(f'Unsupported socket type: {sock}')
-        self.server_socket.setsockopt(zmq.LINGER, 0)
-        self.server_socket.set_hwm(1)
-        self.server_socket.bind(f'tcp://{find_infiniband_ip()}:{port}')
-
-        self.stopper_r = self.zmq_context.socket(zmq.PAIR)
-        self.stopper_r.bind('inproc://sim-server-stop')
-        self.stopper_w = self.zmq_context.socket(zmq.PAIR)
-        self.stopper_w.connect('inproc://sim-server-stop')
-
-        self.buffer = Queue(maxsize=maxlen)
-
-    @property
-    def endpoint(self):
-        endpoint = self.server_socket.getsockopt_string(zmq.LAST_ENDPOINT)
-        endpoint = endpoint.replace('0.0.0.0', gethostname())
-        return endpoint
-
-    def run(self):
-        poller = zmq.Poller()
-        poller.register(self.server_socket, zmq.POLLIN | zmq.POLLOUT)
-        poller.register(self.stopper_r, zmq.POLLIN)
-
-        while True:
-            events = dict(poller.poll())
-
-            if self.stopper_r in events:
-                self.stopper_r.recv()
-                break
-
-            try:
-                payload = self.buffer.get(timeout=0.1)
-            except Empty:
-                continue
-
-            if events[self.server_socket] is zmq.POLLIN:
-                msg = self.server_socket.recv()
-                if msg != b'next':
-                    print(f'Unrecognised request: {msg}')
-                    self.server_socket.send(b'Error: bad request %b' % msg)
-                    continue
-
-            self.server_socket.send_multipart(payload, copy=False)
-
-    def feed(self, data, metadata=None, block=True, timeout=None):
-        """Push data to the sending queue.
-
-        This blocks if the queue already has *maxlen* items waiting to be sent.
-
-        Parameters
-        ----------
-        data : dict
-            Contains train data. The dictionary has to follow the karabo_bridge
-            see :func:`~karabo_bridge.serializer.serialize` for details
-
-        metadata : dict, optional
-            Contains train metadata. If the metadata dict is not provided it
-            will be extracted from 'data' or an empty dict if 'metadata' key
-            is missing from a data source.
-            see :func:`~karabo_bridge.serializer.serialize` for details
-
-        block: bool
-            If True, block if necessary until a free slot is available or
-            'timeout' has expired. If False and there is no free slot, raises
-            'queue.Full' (timeout is ignored)
-
-        timeout: float
-            In seconds, raises 'queue.Full' if no free slow was available
-            within that time.
-        """
-        self.buffer.put(self.serialize(data, metadata),
-                        block=block, timeout=timeout)
-
-    def stop(self):
-        if self.is_alive():
-            self.stopper_w.send(b'')
-            self.join()
-        self.zmq_context.destroy(linger=0)
-
-    def __enter__(self):
-        self.start()
-        return self
-
-    def __exit__(self, *args):
-        self.stop()
+        endpoint = f'tcp://*:{port}'
+        super().__init__(endpoint, sock='REP', maxlen=10,
+                         protocol_version='2.2', dummy_timestamps=False)
 
 
 def serve_files(path, port, source_glob='*', key_glob='*',
@@ -194,17 +64,17 @@ def serve_files(path, port, source_glob='*', key_glob='*',
 
     data = data.select(source_glob, key_glob)
 
-    streamer = ZMQStreamer(port, dummy_timestamps=dummy_timestamps,
-                           use_infiniband=use_infiniband)
+    endpoint = f'tcp://{find_infiniband_ip() if use_infiniband else "*"}:{port}'
+    streamer = ServerInThread(endpoint, dummy_timestamps=dummy_timestamps)
     streamer.start()
     print(f'Streamer started on: {streamer.endpoint}')
     for tid, train_data in data.trains():
         if train_data:
             if append_detector_modules:
                 if source_glob == '*':
-                    warn(" You are trying to stack detector-module sources"
-                    " with a global wildcard (\'*\'). If there are non-"
-                    " detector sources in your run, this will fail.")
+                    warn("You are trying to stack detector-module sources"
+                         "with a global wildcard (\'*\'). If there are non-"
+                         "detector sources in your run, this will fail.")
                 stacked = stack_detector_data(train_data, 'image.data')
                 merged_data = {}
                 # the data key pretends this is online data from SPB
--- extra_data/tests/test_streamer.py
+++ extra_data/tests/test_streamer.py
@@ -10,75 +10,22 @@ from extra_data.export import ZMQStreamer
 from karabo_bridge import Client
 
 
-DATA = {
-    'source1': {
-        'parameter.1.value': 123,
-        'list.of.int': [1, 2, 3],
-        'string.param': 'True',
-        'boolean': False,
-        'metadata': {'timestamp.tid': 9876543210},
-    },
-    'XMPL/DET/MOD0': {
-        'image.data': np.random.randint(255, size=(2, 3, 4), dtype=np.uint8),
-        'something.else': ['a', 'bc', 'd'],
-    },
-}
-
-
-def compare_nested_dict(d1, d2, path=''):
-    for key in d1.keys():
-        if key not in d2:
-            print(d1.keys())
-            print(d2.keys())
-            raise KeyError('key is missing in d2: {}{}'.format(path, key))
-
-        if isinstance(d1[key], dict):
-            path += key + '.'
-            compare_nested_dict(d1[key], d2[key], path)
-        else:
-            v1 = d1[key]
-            v2 = d2[key]
-
-            try:
-                if isinstance(v1, np.ndarray):
-                    assert (v1 == v2).all()
-                elif isinstance(v1, tuple) or isinstance(v2, tuple):
-                    # msgpack doesn't know about complex types, everything is
-                    # an array. So tuples are packed as array and then
-                    # unpacked as list by default.
-                    assert list(v1) == list(v2)
-                else:
-                    assert v1 == v2
-            except AssertionError:
-                raise ValueError('diff: {}{}'.format(path, key), v1, v2)
-
-
 @pytest.fixture(scope='function')
-def server(protocol_version):
-    with ZMQStreamer(2222, maxlen=10, protocol_version=protocol_version) as s:
+def server():
+    with ZMQStreamer(2222, maxlen=10) as s:
         yield s
 
 
-@pytest.mark.parametrize('protocol_version', ['1.0', '2.2'])
-def test_fill_queue(server):
-    for i in range(10):
-        server.feed({str(i): {str(i): i}})
-
-    assert server.buffer.full()
-    with pytest.raises(Full):
-        server.feed({'too much': {'prop': 0}}, block=False)
-
-
-@pytest.mark.parametrize('protocol_version', ['1.0', '2.2'])
 def test_req_rep(server):
     client = Client(server.endpoint)
+    data = {'a': {'b': 1}}
 
     for _ in range(3):
-        server.feed(DATA)
+        server.feed(data)
 
     for _ in range(3):
-        data, metadata = client.next()
-        compare_nested_dict(DATA, data)
+        d, metadata = client.next()
+        assert d == data
 
 
 if __name__ == '__main__':
