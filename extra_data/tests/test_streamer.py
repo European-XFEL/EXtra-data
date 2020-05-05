6350ebea9de0460cf7e5689a9e1fe6b5226a323d
Thomas Michelat <thomas.michelat@xfel.eu>
2020-05-05 19:01:36 +0200

    add test for deprecation; remove redundant test

--- extra_data/tests/test_streamer.py
+++ extra_data/tests/test_streamer.py
@@ -6,22 +6,27 @@ from extra_data.export import ZMQStreamer
 from karabo_bridge import Client
 
 
-@pytest.fixture(scope='function')
-def server():
-    with ZMQStreamer(2222, maxlen=10) as s:
-        yield s
+# @pytest.fixture(scope='function')
+# def server():
+#     with ZMQStreamer(2222, maxlen=10) as s:
+#         yield s
 
 
-def test_req_rep(server):
-    client = Client(server.endpoint)
-    data = {'a': {'b': 1}}
+# def test_req_rep(server):
+#     client = Client(server.endpoint)
+#     data = {'a': {'b': 1}}
 
-    for _ in range(3):
-        server.feed(data)
+#     for _ in range(3):
+#         server.feed(data)
 
-    for _ in range(3):
-        d, metadata = client.next()
-        assert d == data
+#     for _ in range(3):
+#         d, metadata = client.next()
+#         assert d == data
+
+
+def test_deprecated_server():
+    with pytest.deprecated_call():
+        server = ZMQStreamer(2222)
 
 
 if __name__ == '__main__':
