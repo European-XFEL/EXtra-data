e2297229859b0f3d5c7de0420bf18bfb692c084e
Thomas Michelat <thomas.michelat@xfel.eu>
2020-05-06 16:42:43 +0200

    remove commented code

--- extra_data/tests/test_streamer.py
+++ extra_data/tests/test_streamer.py
@@ -6,24 +6,6 @@ from extra_data.export import ZMQStreamer
 from karabo_bridge import Client
 
 
-# @pytest.fixture(scope='function')
-# def server():
-#     with ZMQStreamer(2222, maxlen=10) as s:
-#         yield s
-
-
-# def test_req_rep(server):
-#     client = Client(server.endpoint)
-#     data = {'a': {'b': 1}}
-
-#     for _ in range(3):
-#         server.feed(data)
-
-#     for _ in range(3):
-#         d, metadata = client.next()
-#         assert d == data
-
-
 def test_deprecated_server():
     with pytest.deprecated_call():
         server = ZMQStreamer(2222)
