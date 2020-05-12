5f779b6207275c5740c75911cc0b2b91656fcca9
Thomas Michelat <thomas.michelat@xfel.eu>
2020-05-12 16:08:01 +0200

    add test for combining detector data

--- extra_data/tests/test_streamer.py
+++ extra_data/tests/test_streamer.py
@@ -1,10 +1,6 @@
 """Test streaming data with ZMQ interface."""
 
-import msgpack
-import msgpack_numpy as numpack
-import numpy as np
 import pytest
-from queue import Full
 from subprocess import Popen
 
 from extra_data.export import ZMQStreamer
@@ -12,13 +8,16 @@ from karabo_bridge import Client
 
 
 @pytest.fixture(scope='function')
-def server():
-    with ZMQStreamer(2222, maxlen=10) as s:
-        yield s
+def file_server(mock_fxe_raw_run):
+    port = 3333
+    args = ['karabo-bridge-serve-files', str(mock_fxe_raw_run), str(port)]
+    p = Popen(args)
+    yield f'tcp://localhost:{port}'
+    p.kill()
 
 
 @pytest.fixture(scope='function')
-def file_server(mock_fxe_raw_run):
+def file_server_with_combined_detector(mock_fxe_raw_run):
     port = 3333
     args = [
         'karabo-bridge-serve-files', str(mock_fxe_raw_run), str(port),
@@ -29,20 +28,22 @@ def file_server(mock_fxe_raw_run):
     p.kill()
 
 
-def test_req_rep(server):
-    client = Client(server.endpoint)
-    data = {'a': {'b': 1}}
-
-    for _ in range(3):
-        server.feed(data)
-
-    for _ in range(3):
-        d, metadata = client.next()
-        assert d == data
+def test_server():
+    with pytest.deprecated_call():
+        with ZMQStreamer(2222):
+            pass
 
 
 def test_serve_files(file_server):
-    with Client(file_server, timeout=1) as c:
+    with Client(file_server) as c:
+        data, meta = c.next()
+
+    assert 'FXE_DET_LPD1M-1/DET/0CH0:xtdf' in data
+    assert data['FXE_DET_LPD1M-1/DET/0CH0:xtdf']['image.data'].shape == (128, 1, 256, 256)
+
+
+def test_serve_files_combined_detector(file_server_with_combined_detector):
+    with Client(file_server_with_combined_detector) as c:
         data, meta = c.next()
 
     assert 'FXE_DET_LPD1M-1/DET/APPEND' in data
