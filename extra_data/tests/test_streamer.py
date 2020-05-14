6b82ae8b1ea979172dbb1cd9bc5afc58b23c3d40
Thomas Michelat <thomas.michelat@xfel.eu>
2020-05-14 20:18:29 +0200

    attemps to understand why test hangs on travis

--- extra_data/tests/test_streamer.py
+++ extra_data/tests/test_streamer.py
@@ -7,13 +7,13 @@ from extra_data.export import ZMQStreamer
 from karabo_bridge import Client
 
 
-@pytest.fixture(scope='function')
-def file_server(mock_fxe_raw_run):
-    port = 3333
-    args = ['karabo-bridge-serve-files', str(mock_fxe_raw_run), str(port)]
-    p = Popen(args)
-    yield f'tcp://localhost:{port}'
-    p.kill()
+# @pytest.fixture(scope='function')
+# def file_server(mock_fxe_raw_run):
+#     port = 3333
+#     args = ['karabo-bridge-serve-files', str(mock_fxe_raw_run), str(port)]
+#     p = Popen(args)
+#     yield f'tcp://127.0.0.1:{port}'
+#     p.kill()
 
 
 @pytest.fixture(scope='function')
@@ -24,20 +24,26 @@ def file_server_with_combined_detector(mock_fxe_raw_run):
         '--append-detector-modules'
     ]
     p = Popen(args)
-    yield f'tcp://localhost:{port}'
+    yield f'tcp://127.0.0.1:{port}'
     p.kill()
 
 
-def test_serve_files(file_server):
-    with Client(file_server) as c:
+def test_serve_files(mock_fxe_raw_run):  # file_server):
+    port = 3333
+    args = ['karabo-bridge-serve-files', str(mock_fxe_raw_run), str(port)]
+    p = Popen(args)
+    from time import sleep
+
+    with Client(f'tcp://127.0.0.1:{port}', timeout=5) as c:
         data, meta = c.next()
 
     assert 'FXE_DET_LPD1M-1/DET/0CH0:xtdf' in data
     assert data['FXE_DET_LPD1M-1/DET/0CH0:xtdf']['image.data'].shape == (128, 1, 256, 256)
+    p.kill()
 
 
 def test_serve_files_combined_detector(file_server_with_combined_detector):
-    with Client(file_server_with_combined_detector) as c:
+    with Client(file_server_with_combined_detector, timeout=5) as c:
         data, meta = c.next()
 
     assert 'FXE_DET_LPD1M-1/DET/APPEND' in data
