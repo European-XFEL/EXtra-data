d6e145d83d4b580d87d66ecc204b04bf365b470e
Thomas Kluyver <thomas@kluyver.me.uk>
2020-06-15 16:34:49 +0100

    Don't rely on filenames for lsxfel

--- extra_data/tests/test_lsxfel.py
+++ extra_data/tests/test_lsxfel.py
@@ -4,7 +4,7 @@ from extra_data import lsxfel
 def test_lsxfel_file(mock_lpd_data, capsys):
     lsxfel.summarise_file(mock_lpd_data)
     out, err = capsys.readouterr()
-    assert "480 trains, 128 frames/train" in out
+    assert "480 trains, 1 source" in out
 
 
 def test_lsxfel_run(mock_fxe_raw_run, capsys):
