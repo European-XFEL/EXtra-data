042bfc6fa408b8200cddfa0764bbf5e2c8cf1fcb
ahmedk <karim.ahmed@xfel.eu>
2022-03-31 17:46:57 +0200

    Add conditional keys in control_keys with the correct dtype

--- extra_data/tests/mockdata/agipd.py
+++ extra_data/tests/mockdata/agipd.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from .base import DeviceBase
 
 
@@ -18,9 +16,12 @@ class AGIPDMDL(DeviceBase):
         # A sample of some of the available keys.
         self.control_keys = [
             ('acquisitionTime', 'u2', ()),
-            ('bunchStructure.nPulses', 'u2', ()),
-            ('bunchStructure.firstPulse', 'u2', ()),
-            ('bunchStructure.methodIndex', 'u2', ()),
+            ('bunchStructure/nPulses', 'u2', ()),
+            ('bunchStructure/firstPulse', 'u2', ()),
+            ('bunchStructure/methodIndex', 'u2', ()),
+            ('setupr', 'u2', ()),
+            ('patternTypeIndex', 'u2', ()),
+            ('gainModeIndex', 'u2', ()),
             ('egsgat', 'u2', ()),
             ('g1sgat', 'u2', ()),
             ('g2sgat', 'u2', ()),
@@ -30,26 +31,33 @@ class AGIPDMDL(DeviceBase):
             ('vrfcds', 'u2', ()),
         ]
 
+        if integration_time:
+            self.control_keys.append(('integrationTime', 'u2', ()))
+        if rep_rate:
+            self.control_keys.append(
+                ('bunchStructure/repetitionRate', 'f8', ()))
+        if self.gain_setting:
+            self.control_keys.append(('gain', 'u2', ()))
+
     def write_control(self, f):
         super().write_control(f)
 
-        ctrl_grp = f'CONTROL/{self.device_id}'
-        run_grp = f'RUN/{self.device_id}'
+        ctrl_grp = f'CONTROL/{self.device_id}/'
+        run_grp = f'RUN/{self.device_id}/'
         for grp in [ctrl_grp, run_grp]:
-            length = self.ntrains if grp == ctrl_grp else 1
 
-            f[f"{grp}/setupr/value"] = np.array([32]*length)
-            f[f"{grp}/patternTypeIndex/value"] = np.array([4]*length)
-            f[f"{grp}/gainModeIndex/value"] = np.array([0]*length)
+            g = f[grp]
+
+            g['setupr/value'][()] = 32
+            g['patternTypeIndex/value'][()] = 4
+            g['gainModeIndex/value'][()] = 0
 
             if self.integration_time:
-                f[f"{grp}/integrationTime/value"] = np.array([15]*length)
-
+                g['integrationTime/value'][()] = 15
             if self.rep_rate:
-                f[f"{grp}/bunchStructure/repetitionRate/value"] = np.array(
-                    [4.5]*length)
+                g['bunchStructure/repetitionRate/value'][()] = 4.5
             if self.gain_setting:
-                f[f"{grp}/gain/value"] = np.array([0]*length)
+                g['gain/value'][()] = 0
 
 
 class AGIPD1MFPGA(DeviceBase):
@@ -65,12 +73,12 @@ class AGIPD1MFPGA(DeviceBase):
         ('integrationOffset', 'u4', ()),
         ('integrationPeriod', 'u4', ()),
         ('mask', 'u4', ()),
-        ('performanceStatistics.messagingProblems', '|u1', ()),
-        ('performanceStatistics.enable', '|u1', ()),
-        ('performanceStatistics.processingLatency', 'f4', ()),
-        ('performanceStatistics.maxProcessingLatency', 'u4', ()),
-        ('performanceStatistics.numMessages', 'u4', ()),
-        ('performanceStatistics.maxEventLoopLatency', 'u4', ()),
+        ('performanceStatistics/messagingProblems', '|u1', ()),
+        ('performanceStatistics/enable', '|u1', ()),
+        ('performanceStatistics/processingLatency', 'f4', ()),
+        ('performanceStatistics/maxProcessingLatency', 'u4', ()),
+        ('performanceStatistics/numMessages', 'u4', ()),
+        ('performanceStatistics/maxEventLoopLatency', 'u4', ()),
         ('port', 'i4', ()),
         ('sleepTime', 'f4', ()),
     ]
@@ -79,17 +87,16 @@ class AGIPD1MFPGA(DeviceBase):
 class AGIPD500KFPGA(DeviceBase):
     # A sample of some of the available keys.
     control_keys = [
-        ('highVoltage.actual', 'u2', ()),
-        ('highVoltage.target', 'u2', ()),
+        ('highVoltage/actual', 'u2', ()),
+        ('highVoltage/target', 'u2', ()),
     ]
 
     def write_control(self, f):
         super().write_control(f)
-        ctrl_grp = f'CONTROL/{self.device_id}'
-        run_grp = f'RUN/{self.device_id}'
+        ctrl_grp = f'CONTROL/{self.device_id}/'
+        run_grp = f'RUN/{self.device_id}/'
         for grp in [ctrl_grp, run_grp]:
-            length = self.ntrains if grp == ctrl_grp else 1
-            f[f"{grp}/highVoltage/actual/value"] = np.array([200]*length)
+            f[grp + 'highVoltage/actual/value'][()] = 200
 
 
 class AGIPD1MPSC(DeviceBase):
@@ -103,28 +110,27 @@ class AGIPD1MPSC(DeviceBase):
         self.control_keys = [
             ('applyInProgress', '|u1', ()),
             ('autoRearm', '|u1', ()),
-            ('channels.U0.status', 'i4', ()),
-            ('channels.U0.switch', 'i4', ()),
-            ('channels.U0.voltage', 'f4', ()),
-            ('channels.U0.superVisionMaxTerminalVoltage', 'f4', ()),
-            ('channels.U0.voltageRampRate', 'f4', ()),
-            ('channels.U0.measurementCurrent', 'f4', ()),
-            ('channels.U0.current', 'f4', ()),
-            ('channels.U0.supervisionMaxCurrent', 'f4', ()),
-            ('channels.U0.currentRiseRate', 'f4', ()),
-            ('channels.U0.currentFallRate', 'f4', ()),
-            ('channels.U0.measurementTemperature', 'i4', ()),
-            ('channels.U0.supervisionBehavior', 'i4', ()),
-            ('channels.U0.tripTimeMaxCurrent', 'i4', ()),
-            ('channels.U0.configMaxSenseVoltage', 'f4', ()),
+            ('channels/U0/status', 'i4', ()),
+            ('channels/U0/switch', 'i4', ()),
+            ('channels/U0/voltage', 'f4', ()),
+            ('channels/U0/superVisionMaxTerminalVoltage', 'f4', ()),
+            ('channels/U0/voltageRampRate', 'f4', ()),
+            ('channels/U0/measurementCurrent', 'f4', ()),
+            ('channels/U0/measurementSenseVoltage', 'f4', ()),
+            ('channels/U0/current', 'f4', ()),
+            ('channels/U0/supervisionMaxCurrent', 'f4', ()),
+            ('channels/U0/currentRiseRate', 'f4', ()),
+            ('channels/U0/currentFallRate', 'f4', ()),
+            ('channels/U0/measurementTemperature', 'i4', ()),
+            ('channels/U0/supervisionBehavior', 'i4', ()),
+            ('channels/U0/tripTimeMaxCurrent', 'i4', ()),
+            ('channels/U0/configMaxSenseVoltage', 'f4', ()),
         ]
 
     def write_control(self, f):
         super().write_control(f)
-        ctrl_grp = f'CONTROL/{self.device_id}'
-        run_grp = f'RUN/{self.device_id}'
+        ctrl_grp = f'CONTROL/{self.device_id}/'
+        run_grp = f'RUN/{self.device_id}/'
         if self.bias_voltage:
             for grp in [ctrl_grp, run_grp]:
-                length = self.ntrains if grp == ctrl_grp else 1
-                f[f"{grp}/channels/U0/measurementSenseVoltage/value"] = np.array(  # noqa
-                    [300]*length)
+                f[grp + 'channels/U0/measurementSenseVoltage/value'][()] = 300
