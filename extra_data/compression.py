9f36dc1ecb6359ced2c70faf02f40159512a3631
Thomas Kluyver <thomas.kluyver@xfel.eu>
2025-01-29 16:09:01 +0000

    Some code for testing parallel decompression

--- extra_data/compression.py
+++ extra_data/compression.py
@@ -63,3 +63,77 @@ class ShuffleDeflateDecompressor:
         else:
             # Numpy does the shuffling by copying data between views
             out.reshape((-1, 1)).view(np.uint8)[:] = self.shuffled_view
+
+from multiprocessing.pool import ThreadPool
+from threading import Thread, local
+from queue import Queue
+
+
+def load_v1(kd, threads):
+    # Read & decompress in each thread
+    out = np.empty(kd.shape, dtype=kd.dtype)
+
+    dset_ix_out_ix = []
+    dest_cursor = 0
+    for chunk in kd._data_chunks_nonempty:
+        for i in range(chunk.total_count):
+            dset_ix_out_ix.append((
+                chunk.dataset, (chunk.first + i, 0, 0), out[dest_cursor + i]
+            ))
+
+        dest_cursor += chunk.total_count
+
+    tlocal = local()
+
+    def load_frame(dset, ds_ix, out):
+        try:
+            decomp = tlocal.decompressor
+        except AttributeError:
+            tlocal.decompressor = decomp = ShuffleDeflateDecompressor.for_dataset(dset)
+
+        filter_mask, compdata = dset.id.read_direct_chunk(ds_ix)
+        decomp.apply_filters(compdata, filter_mask, out)
+
+    with ThreadPool(threads) as pool:
+        pool.starmap(load_frame, dset_ix_out_ix)
+
+    return out
+
+
+def load_v2(kd, threads):
+    # Read in the main thread, decompress in workers
+    out = np.empty(kd.shape, dtype=kd.dtype)
+    data_q = Queue(1024)
+
+    workers = [DecompressorThread(data_q, kd._data_chunks[0].dataset) for _ in range(threads)]
+    for w in workers:
+        w.start()
+
+    dest_cursor = 0
+    for chunk in kd._data_chunks_nonempty:
+        for i in range(chunk.total_count):
+            filter_mask, compdata = chunk.dataset.id.read_direct_chunk((chunk.first + i, 0, 0))
+            data_q.put((compdata, filter_mask, out[dest_cursor + i]))
+
+        dest_cursor += chunk.total_count
+
+    for _ in workers:
+        data_q.put(None)  # Time to finish
+
+    for w in workers:
+        w.join(timeout=5)
+
+
+class DecompressorThread(Thread):
+    def __init__(self, input_q, eg_dset):
+        super().__init__(daemon=True)
+        self.input_q = input_q
+        self.decomp = ShuffleDeflateDecompressor.for_dataset(eg_dset)
+
+    def run(self):
+        while True:
+            if (recv := self.input_q.get()) is None:
+                return  # Finished
+
+            compdata, filter_mask, out = recv
+            self.decomp.apply_filters(compdata, filter_mask, out)
